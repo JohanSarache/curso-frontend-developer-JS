e484ee3723cd47f8a0f6a3b4d374ca4afbaff9a6
Johan Sarache <johansarache@gmail.com>
2023-01-23 22:39:25 -0300

    Arreglo del menu

---
+++ script.js
@@ -0,0 +1,11 @@
+var menuEmail = document.querySelector('.navbar-email');
+
+var desktopMenu = document.querySelector('.desktop-menu');
+
+menuEmail.addEventListener('click',toggleDesktopMenu);
+
+function toggleDesktopMenu(){
+    desktopMenu.classList.toggle('inactive');
+
+}
+
